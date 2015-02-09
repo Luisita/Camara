ddec0d860dbd1aefa071b108282bc24ac3be2863
Luisita <luiyi_1414@hotmail.com>
2015-02-08 20:05:49 -0600

    1

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,16 @@
 // Función para iniciar la camara
 function initCamara(){
 // Iniciamos la camara
-navigator.camera.getPicture(guardarFoto, error, { quality : 75,
-destinationType : Camera.DestinationType.FILE_URI,
-sourceType : Camera.PictureSourceType.CAMERA,
-allowEdit : true,
-encodingType: Camera.EncodingType.JPEG });
+    navigator.camera.getPicture(guardarFoto, error, { quality : 75,
+    destinationType : Camera.DestinationType.FILE_URI,
+    sourceType : Camera.PictureSourceType.CAMERA,
+    allowEdit : true,
+    encodingType: Camera.EncodingType.JPEG });
 }
-function guardarFoto(imageURI){
-var imagen = document.getElementById('imagenTomada');
-imagen.style.display = 'block';
-imagen.src = imageURI;
+function GuardarFoto(imageURI){
+    var imagen = document.getElementById('imagenTomada');
+    imagen.style.display = 'block';
+    imagen.src = imageURI;
 }
 function error(){
 alert("Error en la camara");
